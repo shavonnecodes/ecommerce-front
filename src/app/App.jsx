07c711c2f4d7f2f7e1085162b23611f6761c102c
shavonnecobb2 <shavonnedenisecobb@gmail.com>
2020-01-27 17:02:31 -0600

    added a homepage

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,13 +2,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {Homepage  } from './Homepage';
 import { ProductsPage } from './ProductsPage';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+
+library.add(fab);
 
 export const App = () => (
    <>
   <Router>
     <Switch>
       <Route path="/products" component={ ProductsPage } />
+      <Route path="/" component={ Homepage } />
     </Switch>
   </Router>
    </>
--- src/app/Homepage.jsx
+++ src/app/Homepage.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { ProductsList } from './ProductsList';
 import { ProductRepository } from "../api/ProductRepository";
 import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
@@ -13,7 +12,7 @@ import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export class ProductsPage extends React.Component {
+export class Homepage extends React.Component {
 
     productRepository = new ProductRepository();
 
@@ -35,7 +34,7 @@ export class ProductsPage extends React.Component {
                     </Col>
                 </Row>
 
-                <Row>
+                <Row bsPrefix="comm-row">
                     <Col>
                         <div className="comm-header-logo">
                             This will be where the logo will go!
@@ -44,78 +43,58 @@ export class ProductsPage extends React.Component {
                 </Row>
 
                 <Row bsPrefix="comm-row">
-                    <Col>
+                    <Col bsPrefix="comm-link">
                         <Link to="/">OUR STORY</Link>
                     </Col>
-                    <Col>
+                    <Col bsPrefix="comm-link">
                         <NavDropdown title="CAKES" id="basic-nav-dropdown">
                             <NavDropdown.Item href="/products">PRODUCTS</NavDropdown.Item>
                             <NavDropdown.Item href="#">FLAVORS</NavDropdown.Item>
                             <NavDropdown.Item href="#">CARE INSTRUCTIONS</NavDropdown.Item>
                         </NavDropdown>
                     </Col>
-                    <Col>
+                    <Col bsPrefix="comm-link">
                         <NavDropdown title="OCCASIONS" id="basic-nav-dropdown">
                             <NavDropdown.Item href="/products">ALL OCCASIONS</NavDropdown.Item>
                             <NavDropdown.Item href="#">BIRTHDAYS</NavDropdown.Item>
                             <NavDropdown.Item href="#">WEDDINGS</NavDropdown.Item>
                         </NavDropdown>
                     </Col>
-                    <Col>
+                    <Col bsPrefix="comm-link">
                         <Link to="#">FRANCHISING</Link>
                     </Col>
-                    <Col>
+                    <Col bsPrefix="comm-link">
                         <Form inline>
                             <FormControl type="text" placeholder="FIND A BAKERY" className="mr-sm-2" />
                         </Form>
                     </Col>
                 </Row>
 
-                <Row bsPrefix="comm-row">
-                    <Col>
-                        <div className="comm-page-banner-msg">
-                            ENJOY ALL OF OUR BUNDT CAKE PRODUCTS
-                        </div>
-                    </Col>
-                </Row>
 
                 <Row bsPrefix="comm-row">
                     <Col>
-                        <div className="comm-page-blurb-msg">
-                            OUR PROCESS
-                            <br></br>
-                            At Nothing Bundt Cakes our goal is to create a cake that not only reminds you of home, but also opens you to a new world.
-                            <br></br>
-                            And while that's no easy request, the handcrafted recipes of our founders, Dena Tripp and Debbie Shwetz, are more than up
-                            <br></br>
-                            to the task. Each recipe only uses the finest ingredients; real eggs, butter and cream cheese, all to bring you Bundt Cake
-                            <br></br>
-                            perfection. Enjoy!
-                        </div>
-                    </Col>
-                </Row>
-
-                <Row bsPrefix="comm-row">
-                    <Col>
-                        <div className="comm-order-button">
-                            <Button variant="info">Order Online</Button>{' '}
-                        </div>
-                    </Col>
-                </Row>
-
-                <Row bsPrefix="comm-row">
-                    <Col>
-                        <div className="comm-products-title">
-                            WHAT WE OFFER
+                        <div className="comm-slideshow">
+                            <Image src="https://placekitten.com/980/500" />
                         </div>
                     </Col>
                 </Row>
 
                 <Row>
                     <Col>
-                        <div className="comm-products-list">
-                            <ProductsList products={this.state.products} />
+                        <div className="comm-home-banner">
                         </div>
+                    </Col>
+                </Row>
+
+                <Row bsPrefix="comm-image-row">
+                    <Col bsPrefix="comm-image-col">
+                        <a href="#"><Image src="https://placekitten.com/303/190" /></a>
+                    </Col>
+                    <Col bsPrefix="comm-image-col">
+                        <a href="/products"><Image src="https://placekitten.com/303/190" /></a>
+                    </Col>
+                    <Col bsPrefix="comm-image-col">
+                        <a href="#"><Image src="https://placekitten.com/303/190" /></a>
                     </Col>
                 </Row>
 
@@ -126,6 +105,7 @@ export class ProductsPage extends React.Component {
                         <Nav.Link>GIFT CARD</Nav.Link> | <Nav.Link>CONTACT US</Nav.Link> | <Nav.Link>PROP 65 INFO</Nav.Link> | <Nav.Link>PRIVACY POLICY</Nav.Link>
                     </Container>
                 </Navbar>
+
             </Container>
         </>
     }
